ae683240f29e642e2527969009fe781bb2a4a774
Laercio Aparecido Pedroso <laerciogu00@gmail.com>
2022-11-18 22:42:25 -0300

    ajustando nome

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -6,10 +6,9 @@ function Header() {
         <header>
             <div className="dsmeta-logo-container">
                 <img src={logo}alt="DSMeta" />
-                <h1>DSMeta</h1>
+                <h1>Controle de metas</h1>
                 <p>
-                    Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    Desenvolvido por Laercio
                 </p>
             </div>
         </header>
